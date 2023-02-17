26d6db52fb8ca7b748883fcb38ae520e4333aa04
ivan9989-pkj <santanaquintanai@gmail.com>
2023-02-17 19:28:56 +0000

    Proyecto superheroe

---
+++ js.js
@@ -0,0 +1,16 @@
+const Cliente={
+    nombre:"Ivan",
+    apellidos:"Santana Quintana"
+}
+
+
+
+const Usuario={
+    nombre:"Ivan",
+    apellidos : "Santana Quintana",
+    dni:"49983245J",
+    correoelectronico: "date",
+    direccion: "Tamaraceite paseo de los martires"
+}
+
+
